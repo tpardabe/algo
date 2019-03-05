085f75bc2bd1433e8b259a8b0be8695cb4343ec1
Talgat Pardabekov <555tauga@mail.ru>
2019-03-05 15:29:23 +0600

    Create Problem997.java

---
+++ leetcode/Problem997.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Problem997 {
+
+    public int findJudge(int N, int[][] trust) {
+        int[] trustList = new int[N + 1];
+        Set<Integer> map = new HashSet<>();
+        for(int i = 0; i < trust.length; i++) {
+            int judge = trust[i][1];
+            map.add(trust[i][0]);
+            trustList[judge] += 1;
+
+        }
+        int result = -1;
+        for(int i = 1; i < trustList.length; i++) {
+            if(trustList[i] == N-1 && !map.contains(i)) return i;
+        }
+        return result;
+    }
+
+    public static void main(String[]args) {
+        int[][] trust = {};
+        int N = 3;
+        Problem997 problem997 = new Problem997();
+        int result = problem997.findJudge(N, trust);
+        System.out.println("result: "+ result);
+
+    }
+}
