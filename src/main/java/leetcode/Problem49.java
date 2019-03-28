051b91267a1a08d72abb44c1eea6154e1a0f5456
Talgat Pardabekov <TPardabekov@alfabank.kz>
2019-03-28 15:54:50 +0600

    solved problem 49. hash function could be better

--- src/main/java/leetcode/Problem49.java
+++ src/main/java/leetcode/Problem49.java
@@ -12,49 +12,42 @@ public class Problem49 {
         List<List<String>> list = new ArrayList<>();
         Map<Integer, Integer> map = new HashMap();
         for(String s: strs) {
-            int hashCode = hashCode(s);
-            if(!map.containsKey(hashCode))
+            int hashCode = hashCode(s, map, list);
+            if(!map.containsKey(hashCode)) {
                 map.put(hashCode, list.size());
-            if(map.get(hashCode) == list.size()) {
                 list.add(new ArrayList<>());
                 list.get(map.get(hashCode)).add(s);
             } else {
-                String first = list.get(map.get(hashCode)).get(0);
-                if(first.length() != s.length())  {
-                    list.add(new ArrayList<>());
-                    list.get(list.size()-1).add(s);
-                } else {
-                    Map<Character, Integer> tmp = new HashMap<>();
-                    for(int i = 0; i < first.length(); i++) {
-                        char ll = first.charAt(i);
-                        if(!tmp.containsKey(ll)){
-                            tmp.put(ll, 0);
-                        }
-                        tmp.put(ll, tmp.get(ll) +1);
-                    }
-                    boolean isSame = true;
-                    for(int i = 0; i < s.length(); i++) {
-                        if(!tmp.containsKey(s.charAt(i)) || tmp.get(s.charAt(i)) == 0) {
-                            isSame = false;
-                            break;
-                        }
-                        tmp.put(s.charAt(i), tmp.get(s.charAt(i))-1);
-                    }
-                    if(isSame) list.get(map.get(hashCode)).add(s);
-                    else {
-                        list.add(new ArrayList<>());
-                        list.get(list.size()-1).add(s);
-                    }
-                }
+                list.get(map.get(hashCode)).add(s);
             }
-
         }
         return list;
     }
-    public int hashCode(String s) {
+    public int hashCode(String s, Map<Integer, Integer> map, List<List<String>> list) {
         int hashCode = 0;
         for(int i = 0; i < s.length(); i++) {
             hashCode +=s.charAt(i);
+        }
+        while(map.containsKey(hashCode)) {
+            String first = list.get(map.get(hashCode)).get(0);
+            Map<Character, Integer> tmp = new HashMap<>();
+            for(int i = 0; i < first.length(); i++) {
+                char ll = first.charAt(i);
+                if(!tmp.containsKey(ll)){
+                    tmp.put(ll, 0);
+                }
+                tmp.put(ll, tmp.get(ll) +1);
+            }
+            boolean isSame = true;
+            for(int i = 0; i < s.length(); i++) {
+                if(!tmp.containsKey(s.charAt(i)) || tmp.get(s.charAt(i)) == 0) {
+                    isSame = false;
+                    break;
+                }
+                tmp.put(s.charAt(i), tmp.get(s.charAt(i))-1);
+            }
+            if(isSame) break;
+            else hashCode+=10000;
         }
         return hashCode;
     }
