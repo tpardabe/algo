be8ac42028f48def50c479ca15a85f84e1fb2e66
Talgat Pardabekov <555tauga@mail.ru>
2019-05-31 12:39:00 +0600

    solved Problem 148

---
+++ src/main/java/leetcode/Problem148.java
@@ -0,0 +1,20 @@
+package leetcode;
+public class Problem148 {
+    public ListNode sortList(ListNode head) {
+        ListNode sentinel = new ListNode(Integer.MIN_VALUE);
+        sentinel.next = head;
+        recurse(sentinel, head, head);
+        return sentinel.next;
+    }
+    
+    public void recurse(ListNode prev, ListNode slow, ListNode fast) {
+        if()
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
