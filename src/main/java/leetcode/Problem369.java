eb91f9480d07cd0fc637dcfb8a8b103b05790b61
Talgat Pardabekov <555tauga@mail.ru>
2019-06-15 18:53:50 +0600

    solved problem 369

---
+++ src/main/java/leetcode/Problem369.java
@@ -0,0 +1,27 @@
+package leetcode;
+public class Problem369 {
+    /**
+     * @param head: the first Node
+     * @return: the answer after plus one
+     */
+   public ListNode plusOne(ListNode head) {
+        // Write your code here
+        int excess = recurse(head);
+        if(excess > 0) {
+            ListNode tmp = new ListNode(excess);
+            tmp.next = head;
+            head =  tmp;
+        }
+        return head;
+    }
+    
+    private int recurse(ListNode head) {
+        if(head == null) return 0;
+        int excess = recurse(head.next);
+        int sum = 0;
+        if(head.next == null) sum = head.val +1;
+        else sum =  head.val + excess;
+        head.val = sum %10;
+        return sum/10;
+    }
+}
