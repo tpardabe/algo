63ed12c71a0fbcae0a51a49a805c386c0fa15d92
Talgat Pardabekov <555tauga@mail.ru>
2019-06-18 11:57:51 +0600

    solved problem 1021

---
+++ src/main/java/leetcode/Problem1021.java
@@ -0,0 +1,30 @@
+class Problem1021 {
+    public String removeOuterParentheses(String S) {
+       if(S == null || S.isEmpty()) return S;
+        Stack<Character> stack = new Stack();
+        StringBuilder builder = new StringBuilder();
+        int openCount = 0;
+        int closeCount = 0;
+        int startIndex = -1;
+        for(int i = 0; i < S.length(); i++) {
+            char current = S.charAt(i);
+            if(stack.isEmpty()) startIndex = i;
+            stack.push(current);
+            if(current == '(') openCount++;
+            else closeCount++;
+            int count = 0;
+            while(!stack.isEmpty() && current == ')' && openCount == closeCount) {
+                stack.pop();
+                count++;
+                openCount = 0;
+                closeCount = 0;
+            }
+            int size = count;
+            if(stack.isEmpty() && size > 2) {
+                String s = builder.toString();
+                builder.append(S.substring(startIndex+1, i));
+            }
+        }
+        return builder.toString();
+    }
+}
