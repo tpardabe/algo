7ec52597b0d2b054ea3531973a9a1e93b51c282a
Talgat Pardabekov <555tauga@mail.ru>
2019-06-19 16:36:14 +0600

    solved problem 232

---
+++ src/main/java/leetcode/MyQueue.java
@@ -0,0 +1,40 @@
+package leetcode;
+class MyQueue {
+
+    /** Initialize your data structure here. */
+    Stack<Integer> stack = null;
+    public MyQueue() {
+        stack = new Stack();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        if(stack.isEmpty()) {
+            stack.push(x);
+            return;
+        }
+        int number = stack.pop();
+        push(x);
+        stack.push(number);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+       if(stack.isEmpty()) return -1;
+        int number = stack.pop();
+        return number;
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        if(stack.isEmpty()) return -1;
+        int number = stack.pop();
+        stack.push(number);
+        return number;
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack.isEmpty();
+    }
+}
