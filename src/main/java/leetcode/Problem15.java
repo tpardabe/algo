db8f490234b431cee2c1a6ddf43a8abfe549c680
Talgat Pardabekov <555tauga@mail.ru>
2019-10-10 20:22:22 +0600

    update problem 15

--- src/main/java/leetcode/Problem15.java
+++ src/main/java/leetcode/Problem15.java
@@ -17,58 +17,35 @@ public class Problem15 {
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>>  result = new ArrayList<>();
-        Map<Integer, Integer> hash =  new HashMap();
-        Map<String, String> junkMap = new HashMap<>();
-        for(int i = 0 ; i < nums.length; i++) {
-            hash.put(nums[i], i);
-        }
-        for(int i = 0; i < nums.length; i++) {
-            for(int j  = i+1; j < nums.length; j++){
-                if(i == j) continue;
-                int sum = nums[i] + nums [j];
-                int numToFind  = (sum <= 0 ) ? Math.abs(sum) : Math.negateExact(sum);
-                if((sum < 0 || sum == 0) && hash.containsKey(numToFind)){
-                    int index = hash.get(numToFind);
-                    String s = ""+ nums[i] +  nums[j] + numToFind;
-                    if(i == index || j == index || junkMap.containsKey(s)){
-
-                    } else {
-                        add(junkMap, result, nums[i], nums[j], numToFind);
-                    }
-                } else if( sum > 0 && hash.containsKey(numToFind)) {
-                    int index = hash.get(numToFind);
-                    String s = ""+ nums[i] +  nums[j] + numToFind;
-                    if(i ==  index || j == index || junkMap.containsKey(s)){
-
-                    } else {
-                        add(junkMap, result, nums[i], nums[j], numToFind);
-                    }
-                }
+        List<List<Integer>> result = new ArrayList();
+        Set<String> set = new HashSet();
+        Arrays.sort(nums);
+        for(int k = 0; k < nums.length; k++)  {
+            int l = k+1;
+            int r = nums.length -1;
+            int current = nums[k];
+            while(l < r) {
+                 if(current + nums[l] + nums[r] == 0) {
+                        StringBuilder builder = new StringBuilder();
+                        builder.append(current);
+                        builder.append(nums[l]);
+                        builder.append(nums[r]);
+                        String s = builder.toString();
+                        if(!set.contains(s)) {
+                            List<Integer> tmp = new ArrayList();
+                            tmp.add(current);
+                            tmp.add(nums[l]);
+                            tmp.add(nums[r]);
+                            result.add(tmp);
+                            set.add(s);
+                        }
+                        l++;
+                        r--;
+                    } else if(current + nums[l] + nums[r] > 0) {
+                        r--;
+                    } else l++;
             }
         }
         return result;
-
     }
-    private void add(Map<String, String> junkMap, List<List<Integer>>  result, int a, int b, int c) {
-        List<Integer> tmp = new ArrayList<>();
-        tmp.add(a);
-        tmp.add(b);
-        tmp.add(c);
-        if(!contains(junkMap, tmp)) {
-            result.add(tmp);
-        }
-    }
-
-    public boolean contains(Map<String, String> map,List<Integer> tmp) {
-        Collections.sort(tmp);
-        String value = tmp.toString();
-        if(map.containsKey(value.toString())) return true;
-        else {
-            String str = value.toString();
-            map.put(str, str);
-            return false;
-        }
-    }
-
 }
