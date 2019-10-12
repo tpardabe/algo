3ca825a675dbe3e482f46c3e2b0f6f0312d3080f
Talgat Pardabekov <555tauga@mail.ru>
2019-10-12 20:03:20 +0600

    solved problem 344

---
+++ src/main/java/leetcode/Problem344.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+
+class Problem344 {
+    public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length - 1;
+        while(l < r) {
+            char tmp = s[l];
+            s[l++] = s[r];
+            s[r--] = tmp;
+        }
+    }
+}
