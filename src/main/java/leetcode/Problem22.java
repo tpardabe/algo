d07e191a87a48591fd99fb7e7d2b9122de942e98
Talgat Pardabekov <555tauga@mail.ru>
2019-12-28 15:14:14 +0600

    solved problem 22

--- src/main/java/leetcode/Problem22.java
+++ src/main/java/leetcode/Problem22.java
@@ -4,5 +4,25 @@ import java.util.List;
 import java.util.Stack;
 
 public class Problem22 {
-
+  public List<String> generateParenthesis(int n) {
+        Set<String> result = new HashSet();
+        dfs(result, new StringBuilder(), n, 0, 0);
+        return new ArrayList(result);
+    }
+    
+    void dfs(Set<String> result, StringBuilder builder, int n, int l, int r) {
+        if(l > n) return;
+        else if(r == n) result.add(builder.toString());
+        else {
+            builder.append('(');
+            dfs(result, builder, n, l + 1, r);
+            builder.setLength(builder.length() - 1);
+            if(r +1 <= l) {
+                builder.append(')');
+                dfs(result, builder, n, l, r + 1);
+                builder.setLength(builder.length() - 1);
+            }
+            
+        }
+    }
 }
